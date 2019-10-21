bbe19e9c7ec3daa64869ef2a17034ea0dae9228f
uttam-web <uttam.influxiq@gmail.com>
2019-10-21 16:49:39 +0530

    changes by uttam

--- src/app/components/adminheader/adminheader.component.ts
+++ src/app/components/adminheader/adminheader.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
+import { cpus } from 'os';
 @Component({
   selector: 'app-adminheader',
   templateUrl: './adminheader.component.html',
   styleUrls: ['./adminheader.component.css']
 })
 export class AdminheaderComponent implements OnInit {
+  public userdetails:any;
 
-  constructor(public cookieService: CookieService,public router: Router) { }
+  constructor(public cookieService: CookieService,public router: Router) {
+    console.log('this.cookieService.getAll()');
+    console.log(this.cookieService.getAll());
+    this.userdetails = JSON.parse(this.cookieService.get('user_details'));
+    console.log(this.userdetails);
+   }
 
   ngOnInit() {
   }
