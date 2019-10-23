36809c89d79d1924cb7e7779048d152e1c142f88
souravghatak-007 <sourav.influxiq@gmail.com>
2019-10-23 15:59:01 +0530

    businessss

--- src/app/components/purchasedata/purchasedata.component.ts
+++ src/app/components/purchasedata/purchasedata.component.ts
@@ -14,7 +14,9 @@ public dataType:any='';
 public data:any={}
  public dataSource:any;
  public purchaseform:FormGroup;
+ public businessForm:FormGroup;
  public apitoken:any='';
+ public businesssearchCount:any='';
 
  displayedColumns:string[]=['firstname','lastname','email','phone'];
   constructor(public apiservice:ApiService, public cookieservice:CookieService,public fb:FormBuilder) {
@@ -34,7 +36,15 @@ public data:any={}
     Tally_County_Code:['']
 
 
-  })
+  });
+
+  /**Business form genarate */
+  this.businessForm=this.fb.group({
+    Location_Sales_Code:[null],
+    Years_In_Business_Code:[null],
+    Square_Footage_Code:[null],
+    Number_Of_PCs_Code:[null]
+  });
    }
 
    generateapitoken(){
@@ -45,14 +55,14 @@ public data:any={}
    this.apiservice.postDatawithoutToken('apitoken',data).subscribe(res=>{
     let result:any = {};
     result = res;
-  console.log(res);
+  // console.log(res);
   if(result.status=='200'){
     this.apitoken=result.apitoken;
     this.cookieservice.set('apitoken',result.apitoken);
-    console.log(result.apitoken)
+    // console.log(result.apitoken)
   }
   else{
-    console.log("Null")
+    // console.log("Null")
   }
   
   
@@ -127,15 +137,54 @@ filter(Value:any){
           let result:any={};
           result=res;
         })
-
-
-
-
-
-
-      console.log(this.purchaseform.value)
-
-
+    //  console.log(this.purchaseform.value)
     } 
+  }
+
+  /**Business form submit */
+  businessFormSubmit(){
+    
+    let data:any={};
+        data={
+          "apitoken":this.apitoken,
+          "token":this.cookieservice.get('jwttoken'),
+          "condition":this.businessForm.value
+        };
+        this.apiservice.postDatawithoutToken('searchwithcountforbusiness',data).subscribe((res)=>
+        {
+          let result:any={};
+          result=res;
+          if(result.status=='200'){
+            // console.log(result);
+            // console.log(result.data.Response.responseDetails.SearchCount);
+            this.businesssearchCount=result.data.Response.responseDetails.SearchCount;
+          }
+        })
+  }
+  /**Show Business All Data */
+  businessShowAllData(){
+  let  data={
+      apitoken:this.apitoken,
+      token:this.cookieservice.get('jwttoken')
+    }
+    this.apiservice.postDatawithoutToken('data',data).subscribe((res)=>{
+      console.log('dta endpoint hit');
+      let result:any;
+      result=res;
+      console.log(result);
+      // if(result.status=='200'){
+      //   let consumerdatalistraw:any[] = result.data.Response.responseDetails.SearchResult.searchResultRecord;
+      //   this.consumerdata=new MatTableDataSource(result.data.Response.responseDetails.SearchResult.searchResultRecord);
+      //   console.log(this.consumerdata);
+      //   for(let i in consumerdatalistraw){
+      //     for (let j in consumerdatalistraw[i].resultFields){
+      //       console.log(consumerdatalistraw[i].resultFields[j].fieldID);
+      //       this.displayedColumns.push(consumerdatalistraw[i].resultFields[j].fieldID);
+      //       console.log('this.displayedColumns');
+      //       console.log(this.displayedColumns);
+      //     }
+          
+        });
+        
   }
 }
